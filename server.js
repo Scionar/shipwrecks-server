f4e7b0b7b88e1b34f3157f8679ab64f1bfce7721
Joona Viertola <contact@joonaviertola.com>
2018-09-30 12:13:23 +0300

    Add and get all games routes

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require("express");
-const store = require('./store');
-const { addPlayer } = require('./actions');
+const bodyParser = require("body-parser");
+const store = require("./store");
+const { addPlayer, addGame } = require("./actions");
 const app = express();
+
+app.use(bodyParser.json({ type: "application/json" }));
 
 app.get("/", (req, res) => res.send("Shipwrecks server"));
 
 app.post("/player", (req, res) => {
   store.dispatch(addPlayer());
   // Todo: Identify better which user was last added
-  const player = store.getState().players.find(
-    item => item.id === store.getState().nextPlayerIndex - 1
-  );
-  res.json({ authKey: player.authKey })
+  const player = store
+    .getState()
+    .players.find(item => item.id === store.getState().nextPlayerIndex - 1);
+  res.json({ authKey: player.authKey });
+});
+
+app.post("/game", (req, res) => {
+  const gameName = req.body.name;
+  store.dispatch(addGame(gameName));
+  res.json({ id: 0, name: "Argh Meow", status: "Waiting" });
+});
+
+app.get("/game", (req, res) => {
+  res.json({ games: store.getState().games });
 });
 
 app.listen(process.env.PORT, () =>
